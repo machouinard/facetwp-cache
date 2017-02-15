74897c2e0c50801666cade6ed274f19c80b9ed6e
Mark Chouinard <mark@chouinard.me>
2017-02-15 08:36:58 -0800

    auto format

--- cache.php
+++ cache.php
@@ -1,20 +1,22 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 define( 'FACETWP_CACHE', true );
 
 $action = isset( $_POST['action'] ) ? $_POST['action'] : '';
-$data = isset( $_POST['data'] ) ? $_POST['data'] : array();
+$data   = isset( $_POST['data'] ) ? $_POST['data'] : array();
 
 if ( 'facetwp_refresh' == $action ) {
 
-    global $table_prefix;
-    $wpdb = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
-    $wpdb->prefix = $table_prefix;
+	global $table_prefix;
+	$wpdb         = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
+	$wpdb->prefix = $table_prefix;
 
-    // Timestamp
-    $now = date( 'Y-m-d H:i:s' );
+	// Timestamp
+	$now = date( 'Y-m-d H:i:s' );
 
 	//* Check for cookies set during Plan comparision
 	$facetdata = isset( $_COOKIE['facetdata'] ) ? $_COOKIE['facetdata'] : '';
@@ -22,20 +24,21 @@ if ( 'facetwp_refresh' == $action ) {
 	//* Add any comparison IDs to array for use in cache name
 	$data['facetdata'] = $facetdata;
 
-    // MD5 hash
-    $cache_name = md5( json_encode( $data ) );
+	// MD5 hash
+	$cache_name = md5( json_encode( $data ) );
 
-    // Check for a cached version
-    $sql = "
+	// Check for a cached version
+	$sql
+		   = "
     SELECT value
     FROM {$wpdb->prefix}facetwp_cache
     WHERE name = '$cache_name' AND expire >= '$now'
     LIMIT 1";
-    $value = $wpdb->get_var( $sql );
+	$value = $wpdb->get_var( $sql );
 
-    // Return cached version and EXIT
-    if ( null !== $value ) {
-        echo $value;
-        exit;
-    }
+	// Return cached version and EXIT
+	if ( null !== $value ) {
+		echo $value;
+		exit;
+	}
 }
