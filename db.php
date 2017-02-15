c5b12e0139b8b12e94453f9f5b9680e19f438d25
Mark Chouinard <mark@chouinard.me>
2017-02-15 00:36:02 -0800

    Make multisite compatible

--- db.php
+++ db.php
@@ -1,5 +1,28 @@
 <?php
 
-if ( file_exists( WP_CONTENT_DIR . '/plugins/facetwp-cache/cache.php' ) ) {
-    include( WP_CONTENT_DIR . '/plugins/facetwp-cache/cache.php' );
+if ( is_multisite() ) {
+
+	//* Delay loading cache.php if multisite to allow for correct population of $table_prefix
+	add_action( 'ms_loaded', 'fwp_load_cache_file' );
+
+} else {
+
+	//* Load cache.php
+	fwp_load_cache_file();
+
+}
+
+/**
+ * Load plugin's cache.php file
+ *
+ * @return void
+ */
+function fwp_load_cache_file() {
+
+	if ( file_exists( WP_CONTENT_DIR . '/plugins/facetwp-cache/cache.php' ) ) {
+
+		include( WP_CONTENT_DIR . '/plugins/facetwp-cache/cache.php' );
+
+	}
+
 }
